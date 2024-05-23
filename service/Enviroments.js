85e8c1325663100f1212548496333e515d865e6e
unversedsix0 <gaba.olipires@gmail.com>
2024-05-22 23:46:56 -0300

    url para conexao

---
+++ service/Enviroments.js
@@ -0,0 +1,11 @@
+export const Environment = {
+  URL_HG_WEATHER:
+    "https://api.hgbrasil.com/weather?format=json&array_limit=4&fields=only_results,temp,city_name,forecast,max,min,date,temp,moon_phase,condition_slug,description,rain_probability,condition&key=c9488d4e&city_name=",
+  URL_GEO:
+    "http://api.openweathermap.org/geo/1.0/direct?appid=80d6dd44f4a66e0988ebe89d71c43254&q=",
+  URL_CORS: "https://cors-anywhere.herokuapp.com/",
+};
+
+
+
+
