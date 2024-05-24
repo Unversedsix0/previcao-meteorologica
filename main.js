83028e0ac4fcbfc72f7200328ce53c0422b73a66
unversedsix0 <gaba.olipires@gmail.com>
2024-05-23 22:43:49 -0300

    Adicionado navegação para consultar os proximos 3 dias

--- main.js
+++ main.js
@@ -16,6 +16,11 @@ const clima_atual = document.querySelector("#clima-atual");
 const probabilidade_chuva = document.querySelector("#probabilidade-chuva");
 const fase_lua = document.querySelector("#fase-lua");
 const select = document.getElementById("storedQueries");
+const botaoRetroceder = document.getElementById("botao-retroceder");
+const botaoAvancar = document.getElementById("botao-avancar");
+
+var dadosClimaPesquisaAtual;
+var dia = 0;
 
 document
   .getElementById("consulta")
@@ -42,27 +47,53 @@ const map = new Map({
 
 document.addEventListener("DOMContentLoaded", function () {
   atualizaSelect();
+  botaoRetroceder.style.display = "none";
+  botaoAvancar.style.display = "none";
 });
 
 select.addEventListener("change", function (event) {
-  
-  var selectedValue = event.target.value;
-  carregaDados(selectedValue);
-  console.log("Opção selecionada:", selectedValue);
+  var selectedCityId = event.target.value;
+  carregaDados(selectedCityId);
+  botaoAvancar.style.display = "block";
 });
 
-const atualizaCard = (data) => {
-  cidade_nome.textContent = "Cidade:" + data.city_name;
-  date.textContent = data.date;
-  temperatura.textContent = "Temperatura:" + data.temp;
-  temperatura_max.textContent =
-    "Temperatura Máx.:" + data.forecast[0].max + "°C";
-  temperatura_min.textContent =
-    "Temperatura Min.:" + data.forecast[0].min + "°C";
-  clima_atual.textContent = data.description;
-  probabilidade_chuva.textContent =
-    "Probabilidade de Chuva:" + data.forecast[0].rain_probability + "%";
-  fase_lua.textContent = data.moon_phase;
+function atualizarVisibilidadeBotoes() {
+  if (dia > 0) {
+    botaoRetroceder.style.display = "block";
+  } else {
+    botaoRetroceder.style.display = "none";
+  }
+
+  if (dia < 3) {
+    botaoAvancar.style.display = "block";
+  } else {
+    botaoAvancar.style.display = "none";
+  }
+}
+
+// Adiciona um ouvinte de evento para o botão de retroceder
+botaoRetroceder.addEventListener("click", function () {
+  dia--;
+  atualizaCard(dadosClimaPesquisaAtual, dia);
+  atualizarVisibilidadeBotoes();
+});
+
+// Adiciona um ouvinte de evento para o botão de avançar
+botaoAvancar.addEventListener("click", function () {
+  dia++;
+  atualizaCard(dadosClimaPesquisaAtual,dia);
+  atualizarVisibilidadeBotoes();
+});
+
+const atualizaCard = (data, dia) => {
+  cidade_nome.textContent =  data.city_name;
+  date.textContent = data.forecast[dia].date;
+  temperatura.textContent = "Temperatura:" + data.temp + "°C";
+  temperatura_max.textContent = "Temperatura Máx.:" + data.forecast[dia].max + "°C";
+  temperatura_min.textContent = "Temperatura Min.:" + data.forecast[dia].min + "°C";
+  probabilidade_chuva.textContent ="Probabilidade de Chuva:" + data.forecast[dia].rain_probability + "%";
+  clima_atual.innerHTML = `<img src="assets/conditions_slugs/${data.forecast[dia].condition}.svg" alt="Imagem Clima" width="50" height="50">`;
+  fase_lua.innerHTML = `<img src="assets/moon_phases/${data.moon_phase}.png" alt="Imagem Lua" width="50" height="50">`;
 };
 
 const atualizaMapa = (data) => {
@@ -101,8 +132,10 @@ const realizaConsultas = async () => {
     dadosCombinados.id = Date.now().toString();
     dadosCombinados.lat = dataLocalizacao[0].lat;
     dadosCombinados.lon = dataLocalizacao[0].lon;
+    dadosClimaPesquisaAtual = dataClima;
+    console.log("dadosClimaPesquisaAtual", dadosClimaPesquisaAtual);
     atualizaMapa(dadosCombinados);
-    atualizaCard(dadosCombinados);
+    atualizaCard(dadosCombinados, 0);
     armazenaConsulta(dadosCombinados);
   } catch (error) {
     console.log(error);
@@ -111,8 +144,6 @@ const realizaConsultas = async () => {
 
 const armazenaConsulta = (dados) => {
   let storedData = JSON.parse(localStorage.getItem("storedQueries"));
-  let jsonArray;
-
 
   storedData.push(dados);
 
@@ -122,10 +153,9 @@ const armazenaConsulta = (dados) => {
   atualizaSelect();
 };
 
- const atualizaSelect = () =>{
+const atualizaSelect = () => {
   let storedData = JSON.parse(localStorage.getItem("storedQueries"));
   select.innerHTML = "";
-
 
   storedData.forEach((item) => {
     const option = document.createElement("option");
@@ -138,8 +168,9 @@ const armazenaConsulta = (dados) => {
 const carregaDados = (id) => {
   let storedData = JSON.parse(localStorage.getItem("storedQueries"));
   let selectedCity = storedData.find((obj) => obj.id === id);
+  dadosClimaPesquisaAtual = selectedCity;
+  dia = 0
+  console.log("dadosClimaPesquisaAtual LocalStorage", dadosClimaPesquisaAtual);
   atualizaMapa(selectedCity);
-  atualizaCard(selectedCity);
-}
-
-
+  atualizaCard(selectedCity, 0);
+};
